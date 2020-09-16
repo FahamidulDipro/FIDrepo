2b485e124b480e9a80bb45886d76935adaed96c6
FahamidulDipro <69914984+FahamidulDipro@users.noreply.github.com>
2020-09-17 04:23:00 +0600

    new content added

---
+++ Ajax JSON/javascript/randomid.js
@@ -0,0 +1,28 @@
+const para = document.querySelector('.paragraph');
+const button = document.querySelector('.btn');
+var i = 0;
+
+button.addEventListener('click', function () {
+    var peopleId = new XMLHttpRequest();
+    peopleId.open('GET', 'http://www.filltext.com/?rows=5&pretty=true&id={index}&name={firstName}&lastname={lastName}&city={city}&business={business}&email={email}&phone={phone|format}')
+    peopleId.onload = function () {
+        var data = JSON.parse(peopleId.responseText);
+        clear();
+        i++;
+        if (i >= data.length) {
+            i = 0;
+
+        }
+
+        para.innerHTML += `${data[i].name} lives at <b>${data[i].city}</b>.<br><br> This person works at <b>${data[i].business}</b>. <br><br>${data[i].name}'s id number is <b>${data[i].id}</b>.<br><br> Email id is <b>${data[i].email}</b> <br><br> Phone number is <b>${data[i].phone}</b>. <br> `;
+
+
+
+    }
+    peopleId.send();
+})
+
+//to clear the previous id sentence
+function clear() {
+    para.innerHTML = '';
+}
